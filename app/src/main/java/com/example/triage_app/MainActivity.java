01695d1fe2b3e089f39b52cc8dcaa247ec880176
misiek <238903@student.pwr.edu.pl>
2019-04-10 16:39:27 +0200

    "obsluga przesylania"

--- app/src/main/java/com/example/triage_app/MainActivity.java
+++ app/src/main/java/com/example/triage_app/MainActivity.java
@@ -8,10 +8,115 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
+
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.DiscoveryOptions;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
+import com.google.android.gms.nearby.connection.Strategy;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    String SERVICE_ID = "com.example.triage_app";
+    String endID = "";
+
+    //Payload bytesPayload = Payload.fromBytes();
+
+    PayloadCallback payloadReciever = new PayloadCallback() {
+        @Override
+        public void onPayloadReceived(String s, Payload payload) {
+            String system = payload.asBytes().toString();
+            TextView t = findViewById(R.id.procedure_text);
+            t.setText(system);
+            Toast.makeText(getApplicationContext(), "Otrzymano", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPayloadTransferUpdate(String s, PayloadTransferUpdate payloadTransferUpdate) {
+
+        }
+    };
+    private final ConnectionLifecycleCallback connectionLifecycleCallback = new ConnectionLifecycleCallback() {
+        @Override
+        public void onConnectionInitiated(String endpointId, ConnectionInfo connectionInfo) {
+            // Automatically accept the connection on both sides.
+            Nearby.getConnectionsClient(getApplicationContext()).acceptConnection(endpointId, payloadReciever);
+            endID = endpointId;
+            Toast.makeText(getApplicationContext(), "Połączenie ustanowione", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onConnectionResult(String endpointId, ConnectionResolution result) {
+            switch (result.getStatus().getStatusCode()) {
+                case ConnectionsStatusCodes.STATUS_OK:
+                    // We're connected! Can now start sending and receiving data.
+                    break;
+                case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
+                    // The connection was rejected by one or both sides.
+                    break;
+                case ConnectionsStatusCodes.STATUS_ERROR:
+                    // The connection broke before it was able to be accepted.
+                    break;
+                default:
+                    // Unknown status code
+            }
+        }
+
+        @Override
+        public void onDisconnected(String endpointId) {
+            // We've been disconnected from this endpoint. No more data can be
+            // sent or received.
+        }
+    };
+    private final EndpointDiscoveryCallback endpointDiscoveryCallback =
+            new EndpointDiscoveryCallback() {
+                @Override
+                public void onEndpointFound(String endpointId, DiscoveredEndpointInfo info) {
+                    // An endpoint was found. We request a connection to it.
+                    Nearby.getConnectionsClient(getApplicationContext())
+                            .requestConnection(info.getEndpointName(), endpointId, connectionLifecycleCallback)
+                            .addOnSuccessListener(
+                                    (Void unused) -> {
+                                        Toast.makeText(getApplicationContext(), "Połączenie uzyskane", Toast.LENGTH_SHORT).show();
+                                    })
+                            .addOnFailureListener(
+                                    (Exception e) -> {
+                                        // Nearby Connections failed to request the connection.
+                                    });
+                }
+
+                @Override
+                public void onEndpointLost(String endpointId) {
+                    // A previously discovered endpoint has gone away.
+                }
+            };
+
+    private void startDiscovery() {
+        DiscoveryOptions discoveryOptions =
+                new DiscoveryOptions.Builder().setStrategy(Strategy.P2P_CLUSTER).build();
+        Nearby.getConnectionsClient(getApplicationContext())
+                .startDiscovery(SERVICE_ID, endpointDiscoveryCallback, discoveryOptions);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +135,35 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
 
+        Button sendButton = findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random r = new Random();
+                long imei = r.nextLong() % 1000000000000000L;
+                boolean b = r.nextBoolean();
+                int rate = r.nextInt(40) + 10;
+                float capRefillTime = r.nextFloat() * 3f + 0.5f;
+                boolean w = r.nextBoolean();
+                Victim.AVPU c = Victim.AVPU.values()[r.nextInt(Victim.AVPU.values().length)];
+                Victim randomVictim = new Victim(imei, b, rate, capRefillTime, w, c);
+
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                try {
+                    ObjectOutputStream oos = new ObjectOutputStream(bos);
+                    oos.writeObject(randomVictim);
+                    oos.flush();
+                    byte[] data = bos.toByteArray();
+                    Payload bytesPayload = Payload.fromBytes(data);
+                    Nearby.getConnectionsClient(getApplicationContext()).sendPayload(endID, bytesPayload);
+                    Toast.makeText(getApplicationContext(), "Wysłano", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
     }
 
     @Override
@@ -42,28 +176,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    //MENU po prawej stronie, mozna dodac pozniej
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
